266cc9b19ee549d6df3bb2e9825955225d5c321f
andrei <djurinschi.andreiai@gmail.com>
2026-02-15 17:26:02 +0200

    commit

---
+++ front/teachsync-ui/src/app/core/auth/JwtInterceptor.ts
@@ -0,0 +1,43 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import {
+  Observable,
+  throwError,
+} from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.auth.getToken();
+
+    if (token) {
+      req = req.clone({
+        setHeaders: { Authorization: `Bearer ${token}` }
+      });
+    }
+
+    return next.handle(req).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 401) {
+          this.auth.logout();
+          this.router.navigate(['/login']);
+        }
+        return throwError(() => err);
+      })
+    );
+  }
+}
---
+++ front/teachsync-ui/src/app/core/services/auth.service.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import {
+  BehaviorSubject,
+  Observable,
+} from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8080/teachsync/auth';
+  private tokenKey = 'jwt_token';
+
+  private userRoleSubject = new BehaviorSubject<string | null>(null);
+  userRole$ = this.userRoleSubject.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post<{token: string}>(`${this.apiUrl}/login`, {email, password})
+      .pipe(
+        tap(res => {
+          localStorage.setItem(this.tokenKey, res.token);
+          const payload = this.decodeToken(res.token);
+          this.userRoleSubject.next(payload.role);
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem(this.tokenKey);
+    this.userRoleSubject.next(null);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+
+  getRole(): string | null {
+    return this.userRoleSubject.value;
+  }
+
+  private decodeToken(token: string): any {
+    try {
+      const payload = token.split('.')[1];
+      return JSON.parse(atob(payload));
+    } catch {
+      return null;
+    }
+  }
+}
